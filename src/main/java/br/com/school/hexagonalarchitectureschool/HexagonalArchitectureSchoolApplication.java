3b6023763c60dcf93ebaa965fef020d9ed3c2b34
Roberto Luiz Debarba <roberto.debarba@senior.com.br>
2024-05-31 19:34:17 -0300

    Adicionado projeto Spring boot starter

---
+++ src/main/java/br/com/school/hexagonalarchitectureschool/HexagonalArchitectureSchoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.school.hexagonalarchitectureschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/school/hexagonalarchitectureschool/HexagonalArchitectureSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.school.hexagonalarchitectureschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
